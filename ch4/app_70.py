88cf8a0da12b6a1dcf58e3999e09e901f94eff5a
Haggerty <dhaggerty@pronet.com>
2019-11-05 14:02:17 -0600

    70 done

--- ch4/app_70.py
+++ ch4/app_70.py
@@ -1,90 +1,18 @@
-# jsonify is a method (lowercase) not a class
-from flask import Flask, jsonify, request, render_template
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
-# stores list is a list of dicts
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'My item',
-                'price': 15.99
-            }
-        ]
-    }
-]
+api = Api(app)
 
-# JSON NOT a dictionary. 
-# It is a long string.
-# " not '
-# Javascript does not understand Python dictionaries but does understand strings.
-# jsonift converts dictionaries to json
-# CANNOT return a list
+# api works with resouces
+# Every resource has to be a class
+# Inherits from Resource class
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-# POST /store data: {name:}
-@app.route('/store', methods=['POST']) # only accessible by post
-def create_store():
-    # This is the request to this endpoint
-    # Gets that data (store name) back
-    # get json also converts that json string to python dict. 
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    # Append stores list
-    stores.append(new_store)
-    # Has to be jsonified for browser
-    return jsonify(new_store)
-
-
-# GET /store/<string:name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    # Iterate over stores
-    # If store name matches, return it
-    # If non match return error message
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-
-
-# GET /store
-@app.route('/store')
-def get_stores():
-    # The stores list is defined above
-    # Json cannot be a list so we convert to a dict
-    return jsonify({'stores': stores})
-
-
-# POST /store/<string:name>/item {name: price}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found'})
-
-# GET /store/<string:name>/item
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store['items'])
-    return jsonify({'message': 'store not found'})
+# http://127.0.0.1:5000/student/Rolf
+api.add_resource(Student,'/student/<string:name>')
 
 app.run()
